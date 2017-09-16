fa97ade00704beff0d370ceb6d62a4f9c2433ca8
king3366ster <king3366ster@gmail.com>
2017-09-16 16:03:06 +0800

    react test

--- src/components/testCom.js
+++ src/components/testCom.js
@@ -2,13 +2,16 @@ import React from 'react'
 // import {render} from 'react-dom'
 
 class TestCom extends React.Component {
-  setState () {
-
+  constructor (props) {
+    super (props)
+    this.state = {
+      params: '111'
+    }
   }
   render () {
     return (
       <div className="TestCom">
-        Hello World! {this.props.name}
+        Hello World! {this.props.name} {this.state.params}
       </div>
     )
   }
